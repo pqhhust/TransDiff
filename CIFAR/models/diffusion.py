add mc_dropout and TS, fix diffusion to inject variance

CIFAR/models/diffusion.py
@@ -131,6 +131,8 @@ class Diffusion_MLP(nn.Module):
             nn.Dropout(dropout)
         )
 
+        self.sigma = nn.Linear(d_model, 1)
+
     def get_timestep_embedding(self, timesteps, dim=None):
         """
         Create sinusoidal timestep embeddings.
@@ -164,6 +166,7 @@ class Diffusion_MLP(nn.Module):
         
         # Create sinusoidal time embedding and expand to match input dimensions
         t_emb = self.get_timestep_embedding(t)  # [batch_size, d_model]
+        std = torch.clamp(torch.exp(self.sigma(t_emb)), 0, 1e-2)
         t_emb = t_emb.unsqueeze(1).expand(batch_size, seq_len, self.d_model)
         
         # Now both x and t_emb have shape [batch_size, seq_len, d_model]
@@ -171,24 +174,26 @@ class Diffusion_MLP(nn.Module):
         
         mean_x_t = self.mlp(x_t) + x
         # Process through MLP and add residual connection
-        return mean_x_t
+        return mean_x_t, std.view(batch_size, 1, 1).expand(batch_size, seq_len, self.d_model), mean_x_t + std.view(batch_size, 1, 1) * torch.randn_like(mean_x_t)
 
     def forward(self, x, train=False):
         if not train:
             for t in range(self.ViT_depth):
                 t_tensor = torch.tensor([t], device=x.device).expand(x.shape[0])
-                x = self.forward_step(x, t_tensor)
+                x = self.forward_step(x, t_tensor)[-1]
             return x
         else:
             assert isinstance(x, list) and len(x) - 1 == self.ViT_depth, \
                 f"Expected input list length {self.ViT_depth + 1}, got {len(x)}"
             
-            outputs = []
+            means = []
+            stds = []
             for t in range(self.ViT_depth):
                 t_tensor = torch.tensor([t], device=x[t].device).expand(x[t].shape[0])
-                out = self.forward_step(x[t], t_tensor)
-                outputs.append(out)
-            return outputs
+                mean, std, _ = self.forward_step(x[t], t_tensor)
+                means.append(mean)
+                stds.append(std)
+            return means, stds
 
 # class Diffusion_MLP(nn.Module):
 #     def __init__(self, d_model=384, hdim1=384*2, hdim2=384*4, hdim3=384*2, dropout=0.1, ViT_depth=7):

CIFAR/train.py
@@ -99,35 +99,7 @@ def train(train_loader, net, optimizer, epoch, logger, args):
     # Replace writer.add_scalar with wandb.log
     wandb.log({f"Train/{key}": train_log[key].avg for key in train_log}, step=epoch)
 
-# def compute_loss_diffusion(mse_criterion, means_from_diffusion, means_x_minus, stds_from_diffusion, covariances_x_minus):
-#     """
-#     Compute the total loss as the sum of MSE losses between Diffusion and ViT outputs.
-    
-#     Parameters:
-#         mse_criterion (nn.Module): MSE loss function.
-#         diffusion_layer_outputs (list of tensors): Sampled outputs from Diffusion model layers.
-#         vit_layer_outputs (list of tensors): Outputs from ViT model layers.
-    
-#     Returns:
-#         total_loss (Tensor): Sum of MSE losses across all layers.
-#         layer_losses (dict): Dictionary of individual layer MSE losses.
-#     """
-#     means_mse = 0
-#     stds_mse = 0
-
-#     for layer_idx, (mean_diff_out, mean_vit_out) in enumerate(zip(means_from_diffusion, means_x_minus)):
-#         # Compute MSE loss between Diffusion output and ViT output
-#         mean_loss = mse_criterion(mean_diff_out, mean_vit_out)
-#         means_mse += mean_loss
-    
-#     for layer_idx, (std_diff_out, cov_vit_out) in enumerate(zip(stds_from_diffusion, covariances_x_minus)):
-#         # Compute MSE loss between Diffusion output and ViT output
-#         std_loss = mse_criterion(std_diff_out ** 2, cov_vit_out)
-#         stds_mse += std_loss
-    
-#     return means_mse, stds_mse, means_mse + stds_mse
-
-def compute_loss_diffusion(mse_criterion, x_t_from_diffusion, x_t_from_vit):
+def compute_loss_diffusion(mse_criterion, means_from_diffusion, means_x_minus, stds_from_diffusion, covariances_x_minus):
     """
     Compute the total loss as the sum of MSE losses between Diffusion and ViT outputs.
     
@@ -140,92 +112,42 @@ def compute_loss_diffusion(mse_criterion, x_t_from_diffusion, x_t_from_vit):
         total_loss (Tensor): Sum of MSE losses across all layers.
         layer_losses (dict): Dictionary of individual layer MSE losses.
     """
-    total_mse = 0
+    means_mse = 0
+    stds_mse = 0
 
-    for layer_idx, (diff_out, vit_out) in enumerate(zip(x_t_from_diffusion, x_t_from_vit)):
+    for layer_idx, (mean_diff_out, mean_vit_out) in enumerate(zip(means_from_diffusion, means_x_minus)):
         # Compute MSE loss between Diffusion output and ViT output
-        mse_loss = mse_criterion(diff_out, vit_out)
-        total_mse += mse_loss
+        mean_loss = mse_criterion(mean_diff_out, mean_vit_out)
+        means_mse += mean_loss
     
-    return total_mse
-
-def train_diffusion(train_loader, diffusion_model, optimizer, epoch, logger, args, vit_model):
-    """
-    Train the Diffusion model by aligning its layers with the ViT model's layers using MSE loss.
+    for layer_idx, (std_diff_out, cov_vit_out) in enumerate(zip(stds_from_diffusion, covariances_x_minus)):
+        # Compute MSE loss between Diffusion output and ViT output
+        std_loss = mse_criterion(std_diff_out, cov_vit_out)
+        stds_mse += std_loss
     
-    Parameters:
-        train_loader (DataLoader): Training data loader.
-        diffusion_model (nn.Module): Diffusion model to be trained.
-        optimizer (Optimizer): Optimizer for the Diffusion model.
-        epoch (int): Current epoch number.
-        logger (Logger): Logger for logging information.
-        args (Namespace): Command-line arguments.
-        vit_model (nn.Module): Pre-trained ViT model for layer alignment.
-    """
-    diffusion_model.train()
-    vit_model.eval()  # Ensure ViT is in evaluation mode
+    return means_mse, stds_mse, means_mse + stds_mse
 
-    # Freeze ViT model parameters
-    # for param in diffusion_model.parameters():
-    #     param.requires_grad = False
+# def compute_loss_diffusion(mse_criterion, x_t_from_diffusion, x_t_from_vit):
+#     """
+#     Compute the total loss as the sum of MSE losses between Diffusion and ViT outputs.
+    
+#     Parameters:
+#         mse_criterion (nn.Module): MSE loss function.
+#         diffusion_layer_outputs (list of tensors): Sampled outputs from Diffusion model layers.
+#         vit_layer_outputs (list of tensors): Outputs from ViT model layers.
+    
+#     Returns:
+#         total_loss (Tensor): Sum of MSE losses across all layers.
+#         layer_losses (dict): Dictionary of individual layer MSE losses.
+#     """
+#     total_mse = 0
 
-    for param in vit_model.parameters():
-        param.requires_grad = False
-
-    # for param in vit_model.fc.parameters():
-    #     param.requires_grad = True
-
-    # Define loss function
-    mse_criterion = nn.MSELoss() #to be uncomment
-
-    # ce_criterion = nn.CrossEntropyLoss()
-
-    # Initialize training logs
-    train_log = {
-        'Tot. Loss': utils.utils.AverageMeter(),
-        'LR': utils.utils.AverageMeter(),
-    }
-
-    msg = '####### --- Training Epoch {:d} --- #######'.format(epoch)
-    logger.info(msg)
-
-    for i, (inputs, targets) in enumerate(train_loader):
-        inputs, targets = inputs.cuda(), targets.cuda()
-        optimizer.zero_grad()
-        output = vit_model._to_words(inputs)
-        output = vit_model.emb(output)
-        output = output + vit_model.pos_emb
-        # output = diffusion_model(output)
-        # output = vit_model.fc(output.mean(1))
-
-        # loss = ce_criterion(output, targets)
-        with torch.no_grad(): #to be uncomment
-            _, x_t_from_ViT, means_x_minus, _ = vit_model(inputs)
-        x_t_from_diffusion = diffusion_model(x_t_from_ViT, train=True)
-        # print(x_t_from_diffusion[0].shape) # for debug only
-        # print(means_x_minus[0].shape) # for debug only
-        loss = compute_loss_diffusion(mse_criterion, x_t_from_diffusion, means_x_minus)#to be uncomment
-        loss.backward()
-        optimizer.step()
-
-        for param_group in optimizer.param_groups:
-            lr = param_group["lr"]
-            break
-
-        train_log['Tot. Loss'].update(loss.item(), inputs.size(0))
-        train_log['LR'].update(lr, inputs.size(0))
-
-        if i % 100 == 99:
-            log = ['LR : {:.5f}'.format(train_log['LR'].avg)] + [
-                key + ': {:.2f}'.format(train_log[key].avg) for key in train_log if key != 'LR'
-            ]
-            msg = 'Epoch {:d} \t Batch {:d}\t'.format(epoch, i) + '\t'.join(log)
-            logger.info(msg)
-            for key in train_log:
-                train_log[key] = utils.utils.AverageMeter()
-
-    # Replace writer.add_scalar with wandb.log
-    wandb.log({f"Train/{key}": train_log[key].avg for key in train_log}, step=epoch)
+#     for layer_idx, (diff_out, vit_out) in enumerate(zip(x_t_from_diffusion, x_t_from_vit)):
+#         # Compute MSE loss between Diffusion output and ViT output
+#         mse_loss = mse_criterion(diff_out, vit_out)
+#         total_mse += mse_loss
+    
+#     return total_mse
 
 # def train_diffusion(train_loader, diffusion_model, optimizer, epoch, logger, args, vit_model):
 #     """
@@ -260,8 +182,6 @@ def train_diffusion(train_loader, diffusion_model, optimizer, epoch, logger, arg
 
 #     # Initialize training logs
 #     train_log = {
-#         'Mean Loss': utils.utils.AverageMeter(),
-#         'Var Loss': utils.utils.AverageMeter(),
 #         'Tot. Loss': utils.utils.AverageMeter(),
 #         'LR': utils.utils.AverageMeter(),
 #     }
@@ -280,20 +200,18 @@ def train_diffusion(train_loader, diffusion_model, optimizer, epoch, logger, arg
 
 #         # loss = ce_criterion(output, targets)
 #         with torch.no_grad(): #to be uncomment
-#             _, x_t_from_ViT, means_x_minus, covariances_x_minus = vit_model(inputs)
-#         means_from_diffusion, stds_from_diffusion = diffusion_model(x_t_from_ViT, train=True)
+#             _, x_t_from_ViT, means_x_minus, _ = vit_model(inputs)
+#         x_t_from_diffusion = diffusion_model(x_t_from_ViT, train=True)
 #         # print(x_t_from_diffusion[0].shape) # for debug only
 #         # print(means_x_minus[0].shape) # for debug only
-#         means_loss, stds_loss, loss = compute_loss_diffusion(mse_criterion, means_from_diffusion, means_x_minus, stds_from_diffusion, covariances_x_minus)#to be uncomment
+#         loss = compute_loss_diffusion(mse_criterion, x_t_from_diffusion, means_x_minus)#to be uncomment
 #         loss.backward()
 #         optimizer.step()
 
 #         for param_group in optimizer.param_groups:
 #             lr = param_group["lr"]
 #             break
-        
-#         train_log['Mean Loss'].update(means_loss.item(), inputs.size(0))
-#         train_log['Var Loss'].update(stds_loss.item(), inputs.size(0))
+
 #         train_log['Tot. Loss'].update(loss.item(), inputs.size(0))
 #         train_log['LR'].update(lr, inputs.size(0))
 
@@ -308,6 +226,88 @@ def train_diffusion(train_loader, diffusion_model, optimizer, epoch, logger, arg
 
 #     # Replace writer.add_scalar with wandb.log
 #     wandb.log({f"Train/{key}": train_log[key].avg for key in train_log}, step=epoch)
+
+def train_diffusion(train_loader, diffusion_model, optimizer, epoch, logger, args, vit_model):
+    """
+    Train the Diffusion model by aligning its layers with the ViT model's layers using MSE loss.
+    
+    Parameters:
+        train_loader (DataLoader): Training data loader.
+        diffusion_model (nn.Module): Diffusion model to be trained.
+        optimizer (Optimizer): Optimizer for the Diffusion model.
+        epoch (int): Current epoch number.
+        logger (Logger): Logger for logging information.
+        args (Namespace): Command-line arguments.
+        vit_model (nn.Module): Pre-trained ViT model for layer alignment.
+    """
+    diffusion_model.train()
+    vit_model.eval()  # Ensure ViT is in evaluation mode
+
+    # Freeze ViT model parameters
+    # for param in diffusion_model.parameters():
+    #     param.requires_grad = False
+
+    for param in vit_model.parameters():
+        param.requires_grad = False
+
+    # for param in vit_model.fc.parameters():
+    #     param.requires_grad = True
+
+    # Define loss function
+    mse_criterion = nn.MSELoss() #to be uncomment
+
+    # ce_criterion = nn.CrossEntropyLoss()
+
+    # Initialize training logs
+    train_log = {
+        'Mean Loss': utils.utils.AverageMeter(),
+        'Var Loss': utils.utils.AverageMeter(),
+        'Tot. Loss': utils.utils.AverageMeter(),
+        'LR': utils.utils.AverageMeter(),
+    }
+
+    msg = '####### --- Training Epoch {:d} --- #######'.format(epoch)
+    logger.info(msg)
+
+    for i, (inputs, targets) in enumerate(train_loader):
+        inputs, targets = inputs.cuda(), targets.cuda()
+        optimizer.zero_grad()
+        output = vit_model._to_words(inputs)
+        output = vit_model.emb(output)
+        output = output + vit_model.pos_emb
+        # output = diffusion_model(output)
+        # output = vit_model.fc(output.mean(1))
+
+        # loss = ce_criterion(output, targets)
+        with torch.no_grad(): #to be uncomment
+            _, x_t_from_ViT, means_x_minus, covariances_x_minus = vit_model(inputs)
+        means_from_diffusion, stds_from_diffusion = diffusion_model(x_t_from_ViT, train=True)
+        # print(x_t_from_diffusion[0].shape) # for debug only
+        # print(means_x_minus[0].shape) # for debug only
+        means_loss, stds_loss, loss = compute_loss_diffusion(mse_criterion, means_from_diffusion, means_x_minus, stds_from_diffusion, covariances_x_minus)#to be uncomment
+        loss.backward()
+        optimizer.step()
+
+        for param_group in optimizer.param_groups:
+            lr = param_group["lr"]
+            break
+        
+        train_log['Mean Loss'].update(means_loss.item(), inputs.size(0))
+        train_log['Var Loss'].update(stds_loss.item(), inputs.size(0))
+        train_log['Tot. Loss'].update(loss.item(), inputs.size(0))
+        train_log['LR'].update(lr, inputs.size(0))
+
+        if i % 100 == 99:
+            log = ['LR : {:.5f}'.format(train_log['LR'].avg)] + [
+                key + ': {:.2f}'.format(train_log[key].avg) for key in train_log if key != 'LR'
+            ]
+            msg = 'Epoch {:d} \t Batch {:d}\t'.format(epoch, i) + '\t'.join(log)
+            logger.info(msg)
+            for key in train_log:
+                train_log[key] = utils.utils.AverageMeter()
+
+    # Replace writer.add_scalar with wandb.log
+    wandb.log({f"Train/{key}": train_log[key].avg for key in train_log}, step=epoch)
 
 def train_diffusion_stage2(train_loader, diffusion_model, optimizer, epoch, logger, args, vit_model):
     diffusion_model.eval()