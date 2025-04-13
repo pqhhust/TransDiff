ea300f57012af4b6f08f99e22ac1526a537bfdf4
pqhhust <145110752+pqhhust@users.noreply.github.com>
2025-04-13 13:39:00 +0700

    Update train_utils.py

--- CIFAR/utils/train_utils.py
+++ CIFAR/utils/train_utils.py
@@ -37,7 +37,7 @@ def get_args_parser():
     parser.add_argument("--clip-grad-norm", default=1.0, type=float)
     parser.add_argument("--clip-grad-value", default=10.0, type=float)
     parser.add_argument("--accumulation-steps", default=2, type=int)
-    parser.add_argument("--resume-weights", default='last_net_1_diffusion_transformer.pth', type=str, help='Path to the checkpoint to resume from')
+    parser.add_argument("--resume-weights", default=None, type=str, help='Path to the checkpoint to resume from')
     parser.add_argument("--resume-training-state", default='training_state_1_last_diffusion_transformer.pth', type=str, help='Path to the traing_state to resume from')
 
     ## nb of run 
