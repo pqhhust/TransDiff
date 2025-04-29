cb3ccbab3435dbc0386508b4099ce4be72447033
pqhhust <Hung.PQ220030@sis.hust.edu.vn>
2025-04-29 02:53:00 +0000

    init svdkl-cola branch

--- CoLA/val.py
+++ CoLA/val.py
@@ -6,7 +6,7 @@ from sklearn.metrics import matthews_corrcoef
 from utils.temperature_scaling import ModelWithTemperature
 from utils.mc_dropout import mc_dropout
 from data_loader import get_data, get_vocab, DataLoader
-# import gpytorch
+import gpytorch
 
 @torch.no_grad()
 def validation(loader, net, args, method=None):
@@ -40,12 +40,12 @@ def validation(loader, net, args, method=None):
         positional = positional.to(f'cuda:{args.gpu}')
         answers = answers.to(f'cuda:{args.gpu}')
         if args.model == 'svdkl':
-            pass
-            # with gpytorch.settings.num_likelihood_samples(10):
-            #     gp_output = net(inputs, positional, inputs_mask, data)
-            #     output_dist = likelihood(gp_output)
-            #     softmax = output_dist.probs.mean(0)
-            #     output = torch.zeros_like(softmax)
+            # pass
+            with gpytorch.settings.num_likelihood_samples(10):
+                gp_output = net(inputs, positional, inputs_mask, data)
+                output_dist = likelihood(gp_output)
+                softmax = output_dist.probs.mean(0)
+                output = torch.zeros_like(softmax)
         elif args.model == 'mc_dropout':
             softmax = net(inputs, positional, inputs_mask, data)
             output = torch.zeros_like(softmax)
