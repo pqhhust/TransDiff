d1c67f13a09dd69fec910c0dc8ed06080efe8a13
pqhhust <145110752+pqhhust@users.noreply.github.com>
2025-05-06 21:39:45 +0700

    Delete CIFAR directory

--- CIFAR/data/create_val_labels.py
+++
@@ -1,7 +0,0 @@
-with open('./IMAGENET1K/ILSVRC2012_devkit_t12/data/ILSVRC2012_validation_ground_truth.txt', 'r') as gt_file:
-    labels = gt_file.readlines()
-
-with open('./IMAGENET1K/val_labels.txt', 'w') as out_file:
-    for i, label in enumerate(labels):
-        image_name = f'ILSVRC2012_val_{i+1:08d}.JPEG'
-        out_file.write(f'{image_name} {label}')
