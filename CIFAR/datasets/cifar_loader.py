684ec99e0abacdcd9bd1de4b895e561cd38cf4b0
pqhhust <Hung.PQ220030@sis.hust.edu.vn>
2025-05-22 07:57:39 +0000

    final push

--- CIFAR/datasets/cifar_loader.py
+++ CIFAR/datasets/cifar_loader.py
@@ -2,6 +2,9 @@ import numpy as np
 import torchvision.transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
+import torchvision.datasets
+import datasets.svhn_loader as svhn
+import torch
 # import datasets.cifar_loader
 
 def TrainDataLoader(img_dir, transform_train, batch_size):
@@ -13,6 +16,32 @@ def TrainDataLoader(img_dir, transform_train, batch_size):
 def TestDataLoader(img_dir, transform_test, batch_size):
     test_set = ImageFolder(img_dir, transform_test)
     test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False, num_workers=4, drop_last=False)
+    return test_loader
+
+def get_ood_loader(dataset, test_dir, batch_size):
+    if dataset == 'svhn':
+        test_set = svhn.SVHN(test_dir, split='test', transform=torchvision.transforms.ToTensor(), download=False)
+        test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
+                                         shuffle=False, num_workers=4)
+    elif dataset == 'cifar10' or dataset == 'cifar100':
+        if dataset == 'cifar10':
+            norm_mean, norm_std = (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+            nb_cls = 10
+        elif dataset == 'cifar100':
+            norm_mean, norm_std = (0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)
+            nb_cls = 100
+
+        # transformation of the test set
+        transform_test = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+                                                            torchvision.transforms.Normalize(norm_mean, norm_std)])
+
+        test_loader = TestDataLoader(test_dir, transform_test, batch_size)
+    else:
+        test_set = torchvision.datasets.ImageFolder(test_dir, transform=torchvision.transforms.Compose([torchvision.transforms.Resize(32), 
+                                                                                            torchvision.transforms.CenterCrop(32), 
+                                                                                            torchvision.transforms.ToTensor()]))
+        test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=4)
+
     return test_loader
 
 def get_loader(dataset, train_dir, val_dir, test_dir, batch_size):
