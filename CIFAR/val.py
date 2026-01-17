57c7175f34b68067b9cf9fdce115c3b9d6a66373
TranDucManh2310 <tranducmanh23102004@gmail.com>
2026-01-17 09:47:41 +0700

    run 10 samples

--- CIFAR/val.py
+++ CIFAR/val.py
@@ -319,11 +319,15 @@ def validation_text(loader, net, args, time_index=None):
         labels = batch['label'].cuda()
 
         if args.model == 'diffusion_text':
-            output = net(input_ids=input_ids, attention_mask=attention_mask, time_index=time_index)[0]
+            softmaxs = []
+            for _ in range(10):
+                output = net(input_ids=input_ids, attention_mask=attention_mask, time_index=time_index)[0]
+                softmaxs.append(F.softmax(output, dim=1))
+            softmax = torch.mean(torch.stack(softmaxs), dim=0)
+            output = torch.zeros_like(softmax)
         elif args.attn_type == "softmax":
             output = net(x=input_ids, attention_mask=attention_mask)['logits']
 
-        softmax = F.softmax(output, dim=1)
         _, pred_cls = softmax.max(1)
 
         val_log['correct'].append(pred_cls.cpu().eq(labels.cpu().data.view_as(pred_cls)).numpy())
@@ -347,7 +351,7 @@ def validation_text(loader, net, args, time_index=None):
     # calibration measure ece , mce, rmsce
     ece = utils.metrics.calc_ece(val_log['softmax'], val_log['target'], bins=15)
     # brier, nll
-    if args.model == 'svdkl' or args.model == 'mc_dropout':
+    if args.model == 'svdkl' or args.model == 'mc_dropout' or args.model == 'diffusion_text':
         softmax = val_log['softmax'].astype(np.float32)
         targets = val_log['target'].astype(np.int64)
         log_probs = np.log(softmax[range(len(targets)), targets] + 1e-10)
